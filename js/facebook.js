4976023fce7eab9adc89a3ea303997597197b6a4
Priya <priya@Priyas-MacBook-Air.local>
2021-02-28 14:27:50 -0500

    add facebook integration

---
+++ js/facebook.js
@@ -0,0 +1,32 @@
+// facebook integration
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '418235822574294',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v10.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+};
+
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+// check a person's login status
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+}
+
+function statusChangeCallback(fbResponse) {
+    console.log(fbResponse);
+}
---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCSVcD29xOgDhJVocqMo-Au0iQLsI-Prn0",
+    authDomain: "fir-project-7d7cd.firebaseapp.com",
+    projectId: "fir-project-7d7cd",
+    storageBucket: "fir-project-7d7cd.appspot.com",
+    messagingSenderId: "68691260282",
+    appId: "1:68691260282:web:f483e3e1415e2c1a671cc3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
