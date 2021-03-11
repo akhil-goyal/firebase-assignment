e85b7f56eb1b373d6fdda5c438617f74d6639abd
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-11 15:26:12 -0500

    Integrated rich text editor.

--- js/create-thread.js
+++ js/create-thread.js
@@ -152,3 +152,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 })
+
+tinymce.init({
+    selector: '#thread-description',
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family: "Poppins",sans-serif; font-size:14px }'
+});
