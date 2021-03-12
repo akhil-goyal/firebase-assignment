1230758126ee4e776a654ba316ec49f20ca6aff8
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-12 14:20:08 -0500

    Photo span declared.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 function previewFile() {
 
+    let photoSpan = document.getElementById(`profile-photo-span`);
     let preview = document.querySelector('img');
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
 
     reader.addEventListener("load", function () {
         preview.src = reader.result;
-        document.getElementById(`profile-photo-span`).remove()
+        if (photoSpan !== null)
+            photoSpan.remove()
     }, false);
 
     if (file) {
@@ -18,7 +20,7 @@ document.getElementById(`profile-image1`).addEventListener(`click`, () => {
     document.getElementById(`profile-image-upload`).click()
 })
 
-// document.getElementById(`profile-image`).addEventListener(`click`, () => {
-//     document.getElementById(`settings-picture`).click()
-// })
+document.getElementById(`profile-image`).addEventListener(`click`, () => {
+    document.getElementById(`settings-picture`).click()
+})
 
