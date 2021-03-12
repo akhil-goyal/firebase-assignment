d104aa20e6d042251ad2808a204c71634b193345
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2021-03-13 00:28:19 +0530

    [SOURCE MAINTENANCE] Check Sesstion issue resolved

--- js/checkSession.js
+++ js/checkSession.js
@@ -1,23 +1,21 @@
 // //This function will redirect user to dashboard if already authenticated
-// function redirectIfLoggedIn() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-            
-//             // using timeout to allow addUser function to enter user info in firestore before redirect
-//             setTimeout(function(){ 
-//                 window.location = "dashboard.html";
-//              }, 200); 
+function redirectIfLoggedIn() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // using timeout to allow addUser function to enter user info in firestore before redirect
+            setTimeout(function () {
+                window.location = "dashboard.html";
+            }, 200);
+        }
+    });
+}
 
-//         }
-//     });
-// }
+// This function will redirect user to dashboard if already authenticated
+function redirectIfNotLoggedIn() {
+    firebase.auth().onAuthStateChanged(function (user) {
 
-// // This function will redirect user to dashboard if already authenticated
-// function redirectIfNotLoggedIn() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-
-//         if (!user) {
-//             window.location = "login.html";
-//         }
-//     });
-// }
+        if (!user) {
+            window.location = "login.html";
+        }
+    });
+}
