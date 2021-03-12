e6a72876caac993940a4bcf40f001dd1365842f6
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2021-03-13 00:53:33 +0530

    [SOURCE MAINTENANCE] Profile Settings Updated

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
 function previewFile() {
 
     let photoSpan = document.getElementById(`profile-photo-span`);
-    let preview = document.querySelector('img');
+    let previewMain = document.getElementById('profile-image1');
+    let previewSetting = document.getElementById('profile-image');
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
 
     reader.addEventListener("load", function () {
-        preview.src = reader.result;
+        previewMain.src = reader.result
+        previewSetting.src = reader.result
         if (photoSpan !== null)
             photoSpan.remove()
     }, false);
 
     if (file) {
-        reader.readAsDataURL(file);
+        reader.readAsDataURL(file)
     }
 }
 
-document.getElementById(`profile-image1`).addEventListener(`click`, () => {
+previewMain.addEventListener(`click`, () => {
     document.getElementById(`profile-image-upload`).click()
 })
 
-document.getElementById(`profile-image`).addEventListener(`click`, () => {
+previewSetting.addEventListener(`click`, () => {
     document.getElementById(`settings-picture`).click()
 })
 
