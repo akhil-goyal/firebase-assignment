20829e9075e81ff90518514adc9423fdcf9efd30
Nitish Kumar <70220240+nitishkumar90-svg@users.noreply.github.com>
2021-03-13 01:54:30 +0530

    [SOURCE MAINTENANCE] URL Updated

--- js/homepage.js
+++ js/homepage.js
@@ -1,2 +1,2 @@
 const btnExplore = document.getElementById(`btnExplore`)
-btnExplore.addEventListener(`click`, () => { window.location = `/firebase-assignment/pages/login.html` })
+btnExplore.addEventListener(`click`, () => { window.location = `/firebase-assignment/pages/dashboard.html` })
